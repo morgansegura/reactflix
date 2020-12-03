ba5133f366451162956f2e4e62de43d2dca2c640
Morgan Segura <morgansegura@gmail.com>
2020-12-03 10:58:03 -0800

    Creates universal footer component and styled components

---
+++ src/components/footer/styles/footer.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+`;
+
+export const Row = styled.div`
+    display: flex;
+`;
+
+export const Column = styled.div`
+    display: flex;
+`;
+
+export const Link = styled.a`
+    display: flex;
+`;
+
+export const Break = styled.div`
+    display: flex;
+`;
+
+export const Title = styled.div`
+    display: flex;
+`;
+
+export const Text = styled.div`
+    display: flex;
+`;
