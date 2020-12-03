aea8f7988d8add4da2173d6af0db5a0132a859e8
Morgan Segura <morgansegura@gmail.com>
2020-12-02 22:56:17 -0800

    Updates Routes and sets up app layout

---
+++ src/components/jumbotron/styles/jumbotron.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+    display: flex;
+    align-items: center;
+`;
+export const Item = styled.div``;
+export const Container = styled.p``;
---
+++ src/pages/home.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Jumbotron from "../components/jumbotron";
+
+export default function Home() {
+    return <Jumbotron.Container>I am home</Jumbotron.Container>;
+}
