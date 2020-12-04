71d3f0d201bcc2d0cb22c9f61bd8b44646db6225
Morgan Segura <morgansegura@gmail.com>
2020-12-03 21:16:32 -0800

    Creates a header component with a logo and styled components

---
+++ src/components/header/styles/header.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const Background = styled.header``;
+export const Frame = styled.div``;
+export const Logo = styled.div``;
---
+++ src/containers/header.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Header } from "../components";
+
+export function HeaderContainer() {
+    return <Header.Background>Hello</Header.Background>;
+}
